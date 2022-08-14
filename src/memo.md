dcee2d2e89e9ceafd73d5497102b48dc96f02f1e
root <homing-job@yahoo.co.jp>
2022-08-14 19:15:33 +0900

    spacy ga ugokutokomade

--- src/memo.md
+++ src/memo.md
@@ -1,3 +1,9 @@
 # メモ
 ## サーバー起動
 flask run --debugger --reload --host 0.0.0.0
+
+## pipの一括インストール
+pip install -r requirements.txt
+
+## pipの定義を更新
+pip freeze > requirements.txt
--- src/service/spacy.py
+++ src/service/spacy.py
@@ -1,4 +1,25 @@
+import spacy
+from spacy.symbols import obj, VERB, nsubj, iobj
+import deplacy
+
 class Spacy:
     # htmlをspacy形式のdocに変換を行います。
     def html_to_doc(self, html: str):
+        nlp = spacy.load('ja_ginza_electra')
+
+        # doc = nlp('近年、コンピューターやインターネットの進歩により、誰もが大量の文章へアクセスできるようになってきた。')
+        doc = nlp('春夏連覇を狙う大阪桐蔭が聖望学園に大勝し、春夏連覇を成し遂げた１８年以来４年ぶりに１６強入りを決めた。')
+        deplacy.render(doc, Japanese=True)
+
+        for tok in doc:
+            # print('目的語；' + tok.text)
+            # if tok.dep == obj:
+            #   print('目的語；' + tok.text)
+            # if tok.dep == VERB:
+            #   print('動詞：' + tok.text)
+            if tok.dep == nsubj:
+                print('主語:' + tok.text)
+            if tok.dep == iobj:
+                print('熟語:' + tok.text)
+
         return html
